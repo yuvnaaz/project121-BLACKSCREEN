28d0b78480b037cedd94a463016dba6d5b904c8c
yuvnaaz <69728010+yuvnaaz@users.noreply.github.com>
2021-10-11 15:14:32 -0700

    Update blackscreen.py

--- blackscreen.py
+++ blackscreen.py
@@ -1,33 +1,31 @@
-import time
-import cv2 
-import numpy as np 
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_file =cv2.VideoWriter('output.avi',fourcc, 20.0 (640,480))
-cap = cv2.Videowriter(0)
-
-time.sleep(0)
-bg = 0
-
-frame = cv2.resize(frame, (640,480))
-image = cv2.resize(image, (640,480))
-for i in range(60):
-    ret, bg = cap.read()
-
-bg = np.flip(bg, axis=1)
-
-while (cap.isOpened()):
-    ret, img = cap.read()
-    if not ret:
-        break
-    img = np.flip(img,axis=1)
-
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+import cv2  
+import numpy as np  
   
-u_black = np.array([104, 153, 70])
-i_black = np.array([30, 30, 0])
-mask = csv2.inRange(frame, i_black, u_black)
-res = cv2.bitwise_and(frame, frame, mask = mask)
-
-f = frame  - res 
-f = np.where(f == 0, image, f)
+video = cv2.VideoCapture(0) 
+image = cv2.imread("me.jpeg") 
+  
+while True: 
+  
+    ret, frame = video.read() 
+    print(frame)
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
+  
+  
+    u_black = np.array([104, 153, 70]) 
+    l_black = np.array([30, 30, 0]) 
+  
+    mask = cv2.inRange(frame, l_black, u_black) 
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
+  
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
+  
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+  
+video.release() 
+cv2.destroyAllWindows() 
